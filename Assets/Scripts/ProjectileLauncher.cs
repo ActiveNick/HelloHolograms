47c921f773b02c2e9231a1d5300397e55e5cbef5
Nick Landry <nlandry@activenick.net>
2016-10-18 22:09:59 -0400

    Upgraded HoloToolkit + added ability to shoot balls on spatial mesh

---
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+
+    /// <summary>
+    /// Keep Track of the allowed shot interval to throttle users' shots
+    /// </summary>
+    public int shotsPerSec = 2;
+
+    /// <summary>
+    /// Keep Track of the last shot time to throttle users' shots
+    /// </summary>
+    float LastShotTime = 0;
+
+    public Rigidbody m_Sphere;                  // Prefab of the sphere
+    public Transform m_Origin;                  // Where the shells are spawned
+    //public AudioSource m_ShootingAudio;       // Reference to the audio source used to play the shooting audio
+    //public AudioClip m_FireClip;                // Audio that plays when each shot is fired
+    public float m_CurrentLaunchForce = 1.0f;   // The force that will be given to the sphere when the tap gesture is triggered
+
+    //private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+
+    private void OnEnable()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// OnSelect is sent by gesture manager.
+    /// </summary>
+    void OnSelect()
+    {
+        if ((Time.realtimeSinceStartup - LastShotTime) > (1 / shotsPerSec))
+        {
+            LastShotTime = Time.realtimeSinceStartup;
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        // Set the fired flag so only Fire is only called once.
+        //m_Fired = true;
+
+        // Create an instance of the sphere and store a reference to it's rigidbody.
+        Rigidbody ballInstance =
+            Instantiate(m_Sphere, m_Origin.position, m_Origin.rotation) as Rigidbody;
+
+        // Set the sphere's velocity to the launch force in the fire position's forward direction.
+        ballInstance.velocity = m_CurrentLaunchForce * m_Origin.forward;
+
+        // Change the clip to the firing clip and play it.
+        //m_ShootingAudio.clip = m_FireClip;
+        //m_ShootingAudio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
